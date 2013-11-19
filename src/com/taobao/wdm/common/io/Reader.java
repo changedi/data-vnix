843ce821a9d07656f0156e6778ad92c8b416ffc6
changedi <jysoftware@gmail.com>
2013-11-19 21:02:11 +0800

    Signed-off-by: changedi <jysoftware@gmail.com>

--- src/com/taobao/wdm/common/io/Reader.java
+++ src/com/taobao/wdm/common/io/Reader.java
@@ -3,11 +3,16 @@
  */
 package com.taobao.wdm.common.io;
 
+import com.taobao.wdm.common.dataset.DataSet;
+
 /**
  * @author changedi
- *
+ * 
  * 
  */
 public interface Reader {
 
+	void readHeader(Object src, DataSet ds) throws Exception;
+
+	void read(Object src, DataSet ds);
 }
