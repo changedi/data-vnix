2089c9c6bb09477439eb5865088429cf1fa5cbd4
changedi <jysoftware@gmail.com>
2013-11-30 10:25:59 +0800

    add LevenshteinDistance.java
    Signed-off-by: changedi <jysoftware@gmail.com>

--- src/com/taobao/wdm/dm/similarity/LevenshteinDistance.java
+++ src/com/taobao/wdm/dm/similarity/LevenshteinDistance.java
@@ -16,11 +16,38 @@
 package com.taobao.wdm.dm.similarity;
 
 /**
+ * DP for LevenshteinDistance.
+ * @see http://en.wikipedia.org/wiki/Levenshtein_distance
+ * 
  * @author changedi
- *
- * 2013年11月29日
  * 
  */
 public class LevenshteinDistance {
 
+	public static int distance(String s, String t) {
+		if (s == null || s.length() == 0)
+			return t == null ? 0 : t.length();
+		if (t == null || t.length() == 0)
+			return s.length();
+		if (s.equals(t))
+			return 0;
+		int v0[] = new int[t.length() + 1];
+		int v1[] = new int[t.length() + 1];
+
+		for (int i = 0; i < v0.length; i++) {
+			v0[i] = i;
+		}
+		for (int i = 0; i < s.length(); i++) {
+			v1[0] = i + 1;
+			for (int j = 0; j < t.length(); j++) {
+				int cost = (s.charAt(i) == t.charAt(j)) ? 0 : 1;
+				v1[j + 1] = Math.min(Math.min(v1[j] + 1, v0[j + 1] + 1), v0[j]
+						+ cost);
+			}
+			for (int j = 0; j < v0.length; j++) {
+				v0[j] = v1[j];
+			}
+		}
+		return v1[t.length()];
+	}
 }
